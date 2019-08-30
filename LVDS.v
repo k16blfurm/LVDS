f4e16f48c8504c824ed56f0c3586df472284fdfb
Blaine Furman <43151076+k16blfurm@users.noreply.github.com>
2019-08-29 20:40:02 -1000

    Initial commit

---
+++ LVDS.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:30:16 08/22/2019 
+// Design Name: 
+// Module Name:    LVDS 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module LVDS(
+    );
+
+
+endmodule
